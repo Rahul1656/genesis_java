2b151817c79625ec441ffc6bd3f8655bbd984a03
Rahul1656 <61780164+Rahul1656@users.noreply.github.com>
2021-07-27 00:12:48 +0530

    Update InningsMain.java

--- Day1/Pgm4/InningsMain.java
+++ Day1/Pgm4/InningsMain.java
@@ -1,1 +1,23 @@
+package com.ltts1;
+import java.util.Scanner;
+public class InningsMain {
 
+	public static void main(String[] args)
+	{ 
+		String name,Session;
+		int runs;
+		Innings i=new Innings();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the team name:");
+		name=sc.next();
+		i.setTeamname(name);
+		System.out.println("Enter session:");
+		Session=sc.next();
+		i.setInnings(Session);
+		System.out.println("Enter runs:");
+		runs=sc.nextInt();
+		i.setRuns(runs);
+		i.displayInningsDetails();
+	}
+
+}
