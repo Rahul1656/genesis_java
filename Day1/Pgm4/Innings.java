0043b537b6789f33e90b63485bf37a64c10d2c9d
Rahul1656 <61780164+Rahul1656@users.noreply.github.com>
2021-07-27 00:11:56 +0530

    Update Innings.java

--- Day1/Pgm4/Innings.java
+++ Day1/Pgm4/Innings.java
@@ -1,1 +1,42 @@
+package com.ltts1;
 
+public class Innings 
+{
+	private String teamname;
+	private String innings;
+	private int runs;
+	
+	public String getTeamname() {
+		return teamname;
+	}
+	public void setTeamname(String teamname) {
+		this.teamname = teamname;
+	}
+	public String getInnings() {
+		return innings;
+	}
+	public void setInnings(String innings) {
+		this.innings = innings;
+	}
+	public int getRuns() {
+		return runs;
+	}
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+	public void displayInningsDetails()
+	{
+		
+		System.out.println("Name:"+getTeamname());
+		System.out.println("Scored:"+getRuns());
+		if(getInnings().equals("First"))
+		{
+		 System.out.println("Need 201 to win");
+		}
+		else
+		{
+		 System.out.println("Match Ended");
+		}
+	}
+
+}
