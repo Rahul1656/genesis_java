60afd30357f965aa152ff6b99047a325600bf480
Rahul1656 <61780164+Rahul1656@users.noreply.github.com>
2021-07-27 00:18:29 +0530

    Update CustomerMain.java

--- Day1/Pgm3/CustomerMain.java
+++ Day1/Pgm3/CustomerMain.java
@@ -1,1 +1,24 @@
+  
+package com.ltts2;
+import java.util.Scanner;
 
+public class CustomerMain {
+
+	public static void main(String[] args) 
+	{
+        Customer cu=new Customer();
+        String details;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the details:");
+        details=sc.next();
+        String[] arrOfStr = details.split(",");
+		cu.setName(arrOfStr[0]);
+		cu.setAdress(arrOfStr[1]);
+		cu.setMobile(arrOfStr[2]);
+		
+		System.out.println("Name:"+cu.getName());
+		System.out.println("Address:"+cu.getAdress());
+		System.out.println("Phone:"+cu.getMobile());
+	}
+
+}
