ea82e8732f569377e13c240312da27ce51805831
Rahul1656 <61780164+Rahul1656@users.noreply.github.com>
2021-07-27 00:17:54 +0530

    Update Customer.java

--- Day1/Pgm3/Customer.java
+++ Day1/Pgm3/Customer.java
@@ -1,1 +1,28 @@
+package com.ltts2;
 
+public class Customer 
+{
+   private String name;
+   private String adress;
+   private String mobile;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAdress() {
+	return adress;
+}
+public void setAdress(String adress) {
+	this.adress = adress;
+}
+public String getMobile() {
+	return mobile;
+}
+public void setMobile(String mobile) {
+	this.mobile = mobile;
+}
+   
+   
+}
