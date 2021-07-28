ddf74cdbe4a4ccf437fb8bb7524032d2765b8b44
Rahul1656 <61780164+Rahul1656@users.noreply.github.com>
2021-07-28 22:22:30 +0530

    Create square.java

---
+++ Day3/Pgm1/square.java
@@ -0,0 +1,10 @@
+package com.ltts;
+import java.util.Scanner;
+public class square 
+{
+	void CalculateArea(int side)
+	{
+        double value=side*side;
+        System.out.println("Area of square is:"+value);    
+    }
+}
