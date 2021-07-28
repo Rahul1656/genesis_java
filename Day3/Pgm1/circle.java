c325ba94c7a81328e1a87f71048be9a9b624370f
Rahul1656 <61780164+Rahul1656@users.noreply.github.com>
2021-07-28 22:22:03 +0530

    Create circle.java

---
+++ Day3/Pgm1/circle.java
@@ -0,0 +1,10 @@
+package com.ltts;
+import java.util.Scanner;
+public class circle 
+{
+	void CalculateArea(int r)
+	{
+        double value=3.14*r*r;
+        System.out.println("Area of circle is:"+value);
+    }
+}
